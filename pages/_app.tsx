ccc9a7f324719f14e53e1498c177de351a35ce8d
dnjstd <dnjstd@gmail.com>
2023-03-27 23:45:49 +0900

    Fix: withCredentials추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,24 +16,22 @@ declare global {
 }
 
 export default function App({ Component, pageProps }): any {
-  const getUser = useUser((state) => state.getUser);
-  const requestAuthUser = useUser((state) => state.requestAuthUser);
+  // const getUser = useUser((state) => state.getUser);
+  // const requestAuthUser = useUser((state) => state.requestAuthUser);
 
   const kakaoInit = () => {
     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
   };
 
-  useEffect(() => {
-    requestAuthUser();
-  }, []);
+  // useEffect(() => {
+  //   requestAuthUser();
+  // }, []);
 
-  if (getUser.isLoading) return null;
+  // if (getUser.isLoading) return null;
 
   return (
     <ConfigProvider locale={koKR}>
-      <AppLayout
-        component={getUser.login ? <Component {...pageProps} /> : <PageSign />}
-      />
+      <AppLayout component={<Component {...pageProps} />} />
       <Script
         src="https://developers.kakao.com/sdk/js/kakao.js"
         onLoad={kakaoInit}
--- pages/sign/kakao/index.tsx
+++ pages/sign/kakao/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useRouter } from "next/router";
 import { useCallback, useEffect } from "react";
 
@@ -5,37 +6,46 @@ const Kakao = () => {
   const router = useRouter();
   const { code: authCode, error: kakaoServerError } = router.query;
 
-  // const loginHandler = useCallback(
-  //   async (code: string | string[]) => {
-  //     const response: ResponseType = await fetch(
-  //       "http://127.0.0.1:8000/users/login",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           authCode: JSON.stringify(code),
-  //         },
-  //       }
-  //     ).then((res) => res.json());
+  const loginHandler = useCallback(
+    async (code: string | string[]) => {
+      try {
+        const response: any = await axios.post(
+          "http://54.180.58.203:8000/users/login",
+          null,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              authCode: JSON.stringify(code),
+            },
+            withCredentials: true,
+          }
+        );
+        console.log("responseresponse", response);
 
-  //     if (response) {
-  //       console.log("success", response);
-  //       router.push("/main");
-  //     } else {
-  //       // 실패하면 에러 페이지로 리다이렉트
-  //       router.push("/notifications/authentication-failed");
-  //     }
-  //   },
-  //   [router]
-  // );
+        if (!!response.data) {
+          console.log("success>>>>", response);
 
-  // useEffect((): any => {
-  //   if (authCode) {
-  //     loginHandler(authCode);
-  //   } else if (kakaoServerError) {
-  //     router.push("/notifications/authentication-failed");
-  //   }
-  // }, [loginHandler, authCode, kakaoServerError, router]);
+          // router.push('/main');
+        } else {
+          // 실패하면 에러 페이지로 리다이렉트
+          router.push("/notifications/authentication-failed");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [router]
+  );
+
+  useEffect((): any => {
+    if (authCode) {
+      console.log(authCode);
+
+      loginHandler(authCode);
+    } else if (kakaoServerError) {
+      router.push("/notifications/authentication-failed");
+    }
+  }, [loginHandler, authCode, kakaoServerError, router]);
 
   return <div>로그인중...</div>;
 };
